082e1e7bc3df69165cfeddb7b36f1b56d9bb4c8e
Iori YONEJI <fivo.11235813@gmail.com>
2018-06-14 03:11:54 +0900

    add instance ops

--- src/vc2.rs
+++ src/vc2.rs
@@ -5,6 +5,11 @@ use clap::SubCommand;
 pub fn cli<'a>() -> App<'a,'a> {
     SubCommand::with_name("vc2")
         .about("Vultr Could Compute")
+        .subcommand(SubCommand::with_name("run-instances"));
+        .subcommand(SubCommand::with_name("describe-instances"));
+        .subcommand(SubCommand::with_name("stop-instances"));
+        .subcommand(SubCommand::with_name("terminate-instances"));
+        .subcommand(SubCommand::with_name("reboot-instances"));
 }
 
 pub fn handle(args: Option<&ArgMatches>) {
